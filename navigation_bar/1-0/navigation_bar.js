6b337d737ffb839816474e529cddd420d6ec3006
IsabelleRey <isabellrey@gmail.com>
2015-04-16 17:40:19 -0700

    logo and search

--- navigation_bar/1-0/navigation_bar.js
+++ navigation_bar/1-0/navigation_bar.js
@@ -4,63 +4,9 @@ var jq1112 = jq1112 || jQuery;
 
 (function($){
 
-/**
- * Simple watermark plugin
- *
- * @param defaultVal default null
- * @param color default #cccccc
- * @param fontStyle defualt italic
- * @return jQuery object
- */
-$.fn.extend({
-    'fieldWaterMark' :function(options){
-        var defaults = {
-            'defaultVal'    : null,
-            'color'         : '#cccccc'
-        };
-
-        options = $.extend(defaults, options);
-
-        return this.each(function(){
-            var $this = $(this);
-            var currentVal = $.trim($this.val());
-
-            $this.css({
-                'color'         : options.color
-            });
-
-            if(currentVal.length <= 0 && options.defaultVal !== null)
-            {
-                $this.val(options.defaultVal);
-                currentVal = options.defaultVal;
-            }
-
-            $this.focus(function(){
-                if($.trim($(this).val()) === currentVal)
-                {
-                    $(this).val('')
-                    .css({
-                        'color'         : '',
-                        'font-style'    : ''
-                    });
-                }
-            })
-            .blur(function(){
-                if($.trim($(this).val()).length <= 0)
-                {
-                    $(this).css({
-                        'color'         : options.color,
-                        'font-style'    : options.fontStyle
-                    })
-                    .val(currentVal);
-                }
-            });
-        });
-    }
-});
 
 // Menu plugin
-// © PM Leveque
+// © PM Leveque 2010 & Isa 2015
 jQuery.fn.extend({
     cef_nav_menu: function() {
         var menus = $(this);
