ca24fb50b277db45fb91e60da9abbd6744dc4ac7
pmleveque <pmleveque@users.noreply.github.com>
2015-04-16 18:03:25 -0700

    updating database

--- administration2/index.py
+++ administration2/index.py
@@ -17,6 +17,22 @@ class Menu(db.Model):
     author = db.UserProperty()
     shared = db.BooleanProperty()
 
+    # helper to export links to the new MenuNDB
+    def links_as_array(self):
+        return [{'name': link.name, 'url': link.url} for link in
+                sorted(self.link_set, key=lambda link: link.order)
+                ]
+
+    def to_ndb(self):
+        return MenuNDB(
+            name=self.name,
+            date=self.date,
+            special_kind=self.special_kind,
+            author=self.author,
+            shared=self.shared,
+            links=self.links_as_array()
+        )
+
 
 class Link(db.Model):
     name = db.StringProperty(required=True)
@@ -44,29 +60,55 @@ class Navbar(db.Model):
     # cse: custom search engine
     cse_unique_id = db.StringProperty()
 
+    def menus_as_array(self):
+        return [menu.to_ndb() for menu in [self.first_menu,
+                                           self.second_menu,
+                                           self.third_menu,
+                                           self.fourth_menu] if menu]
+
+    def to_ndb(self):
+        return NavbarNDB(
+            code=self.code,
+            name=self.name,
+            author=self.author,
+            date=self.date,
+            menus=self.menus_as_array(),
+            settings=self.settings,
+            cse_unique_id=self.cse_unique_id
+        )
+
 
 class MenuNDB(ndb.Model):
     name = ndb.StringProperty(required=True)
     date = ndb.DateTimeProperty(auto_now_add=True)
-    # use pickle.dumps() or .loads()
     links = ndb.PickleProperty()
     # special_kind sert a identifier les menus qui servent
-    #  a composer les menus speciaux (ressources, dioceses...)
+    # a composer les menus speciaux (ressources, dioceses...)
     special_kind = ndb.StringProperty()
     author = ndb.UserProperty()
     shared = ndb.BooleanProperty()
+
+
+class MenuLinkNDB(ndb.Model):
+    img_path = ndb.StringProperty(required=True)
+    link = ndb.StringProperty(required=True)
+
+    @classmethod
+    def all():
+        return super.query().fetch(1000)
 
 
 class NavbarNDB(ndb.Model):
     # TODO: should be lowercase (will be the script filename)
     code = ndb.StringProperty(required=True)
     name = ndb.StringProperty(required=True)
-    author = ndb.UserProperty()
+    author = ndb.UserProperty()  # TODO: required
     date = ndb.DateTimeProperty(auto_now_add=True)
     menus = ndb.StructuredProperty(MenuNDB, repeated=True)
-    settings = db.StringListProperty()
+    menus_links = ndb.StructuredProperty(MenuLinkNDB, repeated=True)
+    settings = ndb.PickleProperty()
     # cse: custom search engine
-    cse_unique_id = db.StringProperty()
+    cse_unique_id = ndb.StringProperty()
 
 
 class Administrator(db.Model):
